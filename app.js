2eac78a4e91e1f5532ea8496e70170ba3dba1fd5
Yascan <edehas46@gmail.com>
2020-12-07 00:19:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const movie = require('./routes/movie');
 
 const app = express();
+
+//db connection
+const db = require('./helper/db.js')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +18,12 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/movies', movie);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
