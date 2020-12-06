4af7163c17f1316d7b6d064bfa9d8d58155f3d23
Yascan <edehas46@gmail.com>
2020-12-07 00:19:39 +0300

    Create Movie.js

---
+++ models/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+    title: {
+        type: 'string',
+        required: true
+    },
+    category: String,
+    country: String,
+    year: Number,
+    imdb_score: Number,
+    director_id: Schema.Types.ObjectId,
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('movie', MovieSchema)
